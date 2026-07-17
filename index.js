f05d75897c304fb2f980eec87236b42f3140b784
Fredrick M. Morara <momanyifredm@gmail.com>
2026-07-17 23:26:39 +0300

    Stage 3: create with validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
 let tasks = [
   { id: 1, title: "Learn Express", done: true },
   { id: 2, title: "Build CRUD API", done: false },
@@ -36,6 +38,28 @@ app.get("/tasks/:id", (req, res) => {
   } else {
     res.status(404).json({ error: "Task not found" });
   }
+});
+
+app.post("/tasks", (req, res) => {
+    const title = req.body.title;
+    if (!title || title.trim() === "") {
+        return res.status(400).json({ error: "Title is required" });
+    }
+    let nextId = 1;
+    if (tasks.length > 0) {
+        const highestId = Math.max(...tasks.map((task) => task.id));
+        nextId = highestId + 1;
+    }   
+
+    const newTask = {
+        id: nextId,
+        title: title,
+        done: false
+    }
+
+    tasks.push(newTask);
+
+    res.status(201).json(newTask)
 });
 
 app.listen(port, () => {
