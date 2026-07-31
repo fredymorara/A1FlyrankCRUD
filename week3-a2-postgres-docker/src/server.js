b78d567fb8d4c0e35a72daef4a5c1a2599a98bc4
Fredrick M. Morara <momanyifredm@gmail.com>
2026-07-31 03:48:02 +0300

    Stretch Goal: Added Redis to Docker Compose and pinged it

--- week3-a2-postgres-docker/src/server.js
+++ week3-a2-postgres-docker/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import fs from "fs";
 import taskRoutes from "./routes/taskRoutes.js";
+import { createClient } from "redis";
 
 const app = express();
 const port = 3000;
@@ -10,15 +11,15 @@ const port = 3000;
 app.use(express.json());
 
 // Swagger Docs setup
-const swaggerDocument = JSON.parse(fs.readFileSync('./openapi.json', 'utf8'));
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocument = JSON.parse(fs.readFileSync("./openapi.json", "utf8"));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Root & Health Endpoints
 app.get("/", (req, res) => {
   res.json({
     name: "AI Todo List API (MVC Architecture)",
     version: "2.0.0",
-    endpoints: ["/tasks", "/docs"]
+    endpoints: ["/tasks", "/docs"],
   });
 });
 
@@ -34,6 +35,16 @@ app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
+// Connect to Redis
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+await redisClient.connect();
+const pingResponse = await redisClient.ping();
+console.log(`Redis says: ${pingResponse}`);
 // Start Server
 app.listen(port, () => {
   console.log(`Server is running in MVC mode at http://localhost:${port}`);
